5867f32a74a62b313bbb8abed67396abbf9b74cb
redsin <redsin@hushmail.com>
2014-02-22 13:37:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+edentest
+========
+
+Sahana Eden Test Cases for the Robot Framework
---
+++ config.example.py
@@ -0,0 +1,12 @@
+# Configuration file for EdenTest
+
+# The server name, e.g. "localhost:8000"
+SERVER = "localhost:8000"
+# The web2py application name, e.g. "eden"
+APPNAME = "eden"
+# The browser, e.g. "Firefox" or "Chrome"
+BROWSER = "Firefox"
+# The user name to test with
+VALID_USER = "admin@example.com"
+# The password for the test user
+VALID_PASSWORD = "testing"
